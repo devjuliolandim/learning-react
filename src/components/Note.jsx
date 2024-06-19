8e885b04c5960649ea30b9039af63df7b6b2ddc6
Júlio César Landim <127707529+devjuliolandim@users.noreply.github.com>
2024-06-19 15:22:55 -0300

    keeper app components done!

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note(){
+    return(
+        <div className="note">
+            <h1>Lorem ipsum</h1>
+            <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+    );
+}
+
+export default Note;
